826bb17365e853b9d04f0a8331e9f4a6cb9ddd2b
Bartosz Lozinski <lozinski.bart@gmail.com>
2025-03-01 16:21:58 +0100

    CMake with clang and custom modules work in a simpler way, thanks to ninja

--- buildScript.sh
+++ buildScript.sh
@@ -13,15 +13,26 @@ if ! [ -f iostream.pcm ]; then
     cd ..
 fi
 
-echo "Building main project"
-
-if ! [ -d build ]; then
-    mkdir build
+#Ninja is required to build custom modules with clang in a simpler way instead of directly linking obj files
+if ! dpkg-query -l ninja-build &>/dev/null; then
+    echo "ninja-build is not installed. Installing..."
+    sudo apt update && sudo apt install -y ninja-build
+else
+    echo "ninja-build is already installed."
 fi
 
-cd build
-cmake ..
-make -j
+# Check if clang-tools-18 is installed
+if ! dpkg-query -l clang-tools-18 &>/dev/null; then
+    echo "clang-tools-18 is not installed. Installing..."
+    sudo apt update && sudo apt install -y clang-tools-18
+else
+    echo "clang-tools-18 is already installed."
+fi
+
+echo "Building main project"
+
+cmake -S . -B build -G Ninja -DCMAKE_CXX_COMPILER=clang++-18
+cmake --build build
 
 echo "Modules building - FINISHED"
 
--- chybaDziala.sh
+++ chybaDziala.sh
@@ -3,4 +3,22 @@ clang++-18 -std=c++20 main.cpp -fprebuilt-module-path=. -c -o main.o
 clang++-18 -std=c++20 Math.pcm -c -o Math.o
 clang++-18 -std=c++20 main.o Math.o -o test
 
+
+#test bo chyba dziala:
+# download ninja:
+# sudo apt install ninja-build  # Ubuntu/Debian
+
+# install clang-tools
+# sudo apt install clang-tools-18
+# clang tools required for ninja
+# while ninja required for Clang CMake
+
+# reconfigure cmake to use ninja
+# cmake -S . -B build -G Ninja -DCMAKE_CXX_COMPILER=clang++-18
+# cmake --build build
+# or find with:
+# find / -name clang-scan-deps-18 2>/dev/null
+
+
 #https://www.youtube.com/watch?v=n3mJY8ZTSAI
+#https://www.youtube.com/watch?v=9OwQZ-LRb0c
