cfb688bc9955d6e7bf96916fd1de1d8a7c0046f6
Bartosz Lozinski <lozinski.bart@gmail.com>
2025-03-01 16:24:14 +0100

    Revert "CMake with clang and custom modules work in a simpler way, thanks to ninja"
    This reverts commit 826bb17365e853b9d04f0a8331e9f4a6cb9ddd2b.

--- buildScript.sh
+++ buildScript.sh
@@ -13,26 +13,15 @@ if ! [ -f iostream.pcm ]; then
     cd ..
 fi
 
-#Ninja is required to build custom modules with clang in a simpler way instead of directly linking obj files
-if ! dpkg-query -l ninja-build &>/dev/null; then
-    echo "ninja-build is not installed. Installing..."
-    sudo apt update && sudo apt install -y ninja-build
-else
-    echo "ninja-build is already installed."
-fi
-
-# Check if clang-tools-18 is installed
-if ! dpkg-query -l clang-tools-18 &>/dev/null; then
-    echo "clang-tools-18 is not installed. Installing..."
-    sudo apt update && sudo apt install -y clang-tools-18
-else
-    echo "clang-tools-18 is already installed."
-fi
-
 echo "Building main project"
 
-cmake -S . -B build -G Ninja -DCMAKE_CXX_COMPILER=clang++-18
-cmake --build build
+if ! [ -d build ]; then
+    mkdir build
+fi
+
+cd build
+cmake ..
+make -j
 
 echo "Modules building - FINISHED"
 
